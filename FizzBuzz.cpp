1e02a6e6bbbd15334fbc7218ad47107c62a3e6d3
tylertran349 <104945572+tylertran349@users.noreply.github.com>
2022-06-01 16:20:55 -0700

    Add files via upload

---
+++ FizzBuzz.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Enter an integer: ";
+	cin >> n;
+	for(int i = 1; i <= n; i++) {
+		if((i % 3 == 0) && (i % 5 == 0)) {
+			cout << "FizzBuzz, ";
+		} else if (i % 3 == 0) {
+			cout << "Fizz, ";
+		} else if (i % 5 == 0) {
+			cout << "Buzz, ";
+		} else {
+			cout << i << ", ";
+		}
+	}
+}
+
